c1f1105fce6327bb3cd80876ec6d56ed42d4cc6b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-26 18:56:57 +0700

    Add /api/courses route

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => { console.log('Listening on port 3000...'); })
+app.listen(3000, () => { console.log('Listening on port 3000...'); });
