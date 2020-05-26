28cd95d16d96db9bccac067830c55cef9e0dfdd1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-26 19:47:16 +0700

    Add middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json);
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
+    { id: 3, name: 'course3' },
 ];
 
 app.get('/', (req, res) => {
