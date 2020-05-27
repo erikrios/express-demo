0100338c5b82bca8962ec55f9b66ea65b7f9bb8a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-27 19:24:04 +0700

    Add PUT method

--- index.js
+++ index.js
@@ -19,15 +19,10 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) {
-        // 404 Bad Request
-        res.status(404).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if (error) {
+        // 400 Bad Request
+        res.status(400).send(result.error.details[0].message);
         return;
     }
 
@@ -39,11 +34,34 @@ app.post('/api/courses', (req, res) => {
     res.send(courses)
 });
 
+app.put('/api/courses/:id', (req, res) => {
+    const course = course.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.');
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        // 400 Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with the given ID was not found.');
     res.send(course);
 });
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => { console.log(`Listening on port ${port}...`); });
