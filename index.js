7e48f7240da732821cc523edaeb39e327205d571
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-27 18:58:41 +0700

    Add Joi to validate the input

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -18,9 +19,15 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    if (!req.body.name || req.body.name.length < 3) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
         // 404 Bad Request
-        res.status(404).send('Name is required and should be minimum 3 characters.');
+        res.status(404).send(result.error.details[0].message);
         return;
     }
 
